c7cf8ac2cd3160209d5dc839228353914ae3481b
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-03 03:07:26 +0530

    Updated H&M

--- hnm/src/Components/Homepage.jsx
+++ hnm/src/Components/Homepage.jsx
@@ -86,57 +86,57 @@ function Homepage() {
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10201.png?imwidth=124" alt="" />
 								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<p className="pop-name">Trousers</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10244.png?imwidth=124" alt="" />
 								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<p className="pop-name">Clothes</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
-								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10231.png?imwidth=124" alt="" />
+								<p className="pop-cat">Men</p>
+								<p className="pop-name">Hoodies & Sweatshirts</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
-								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10221.png?imwidth=124" alt="" />
+								<p className="pop-cat">Men</p>
+								<p className="pop-name">T-shirts & Tops</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10220.png?imwidth=124" alt="" />
 								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<p className="pop-name">Accessories</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10208.png?imwidth=124" alt="" />
 								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<p className="pop-name">Shirts & Blouses</p>
 							</div>
 						</div>
 
 						<div className="pop-cat-content">
 							<div className="pop-cat-layout">
-								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10202.png?imwidth=124" alt="" />
+								<img src="https://image.hm.com/content/dam/TOOLBOX/project-me/personalizedentrances/10216.png?imwidth=124" alt="" />
 								<p className="pop-cat">Ladies</p>
-								<p className="pop-name">Tops & T-shirts</p>
+								<p className="pop-name">Dresses</p>
 							</div>
 						</div>
 					</div>
@@ -178,15 +178,136 @@ function Homepage() {
 						<button>Sport</button>
 					</div>
 
-					<div className="arrivals-poster">
-						<div className="">
-							<div className="arrivals-pic">
-								heart
+					<div className="arrivals-layout">
+						<div className="arrivals-poster">
+							<div className="arrivals-pic-1">
+								<div className="heart-container">
+									<div className="heart-logo-regular-0"><i class="fa-solid fa-heart fa-xl"></i></div>
+									<div className="heart-logo-regular"><i class="fa-regular fa-heart fa-xl"></i></div>
+									<div className="heart-logo-solid"><i class="fa-solid fa-heart fa-xl"></i></div>
+								</div>
 							</div>
-							<p>Tie-neck maxi dress</p>
-							<p>Rs.5999.00</p>
+							<p className="arrivals-text">Tie-neck maxi dress</p>
+							<p className="arrivals-price">Rs.5,999.00</p>
 						</div>
 
+						<div className="arrivals-poster">
+							<div className="arrivals-pic-2">
+								<div className="heart-container">
+									<div className="heart-logo-regular-0"><i class="fa-solid fa-heart fa-xl"></i></div>
+									{/* <div className="heart-logo-regular"><i class="fa-regular fa-heart fa-xl"></i></div> */}
+									<div className="heart-logo-solid"><i class="fa-solid fa-heart fa-xl"></i></div>
+								</div>
+							</div>
+							<p className="arrivals-text">Turtleneck thong body</p>
+							<p className="arrivals-price">Rs.799.00</p>
+						</div>
+
+						<div className="arrivals-poster">
+							<div className="arrivals-pic-3">
+								<div className="heart-container">
+									<div className="heart-logo-regular-0"><i class="fa-solid fa-heart fa-xl"></i></div>
+									<div className="heart-logo-regular"><i class="fa-regular fa-heart fa-xl"></i></div>
+									<div className="heart-logo-solid"><i class="fa-solid fa-heart fa-xl"></i></div>
+								</div>
+							</div>
+							<p className="arrivals-text">Sheer lace top</p>
+							<p className="arrivals-price">Rs.4,999.00</p>
+						</div>
+
+						<div className="arrivals-poster">
+							<div className="arrivals-pic-4">
+								<div className="heart-container">
+									<div className="heart-logo-regular-0"><i class="fa-solid fa-heart fa-xl"></i></div>
+									<div className="heart-logo-regular"><i class="fa-regular fa-heart fa-xl"></i></div>
+									<div className="heart-logo-solid"><i class="fa-solid fa-heart fa-xl"></i></div>
+								</div>
+							</div>
+							<p className="arrivals-text">Rib-knit mermaid skirt</p>
+							<p className="arrivals-price">Rs.2,299.00</p>
+						</div>
+					</div>
+
+					<div className="slick-dot">
+						<div className="circle-logo">
+							{/* <div className="circle-logo-regular"><i class="fa-regular fa-circle"></i></div> */}
+							<div className="circle-logo-regular"><i class="fa-solid fa-circle-dot"></i></div>
+							<div className="circle-logo-solid"><i class="fa-solid fa-circle"></i></div>
+						</div>
+
+						<div className="circle-logo">
+							{/* <div className="circle-logo-regular"><i class="fa-regular fa-circle"></i></div> */}
+							<div className="circle-logo-regular"><i class="fa-solid fa-circle-dot"></i></div>
+							<div className="circle-logo-solid"><i class="fa-solid fa-circle"></i></div>
+						</div>
+
+						<div className="circle-logo">
+							{/* <div className="circle-logo-regular"><i class="fa-regular fa-circle"></i></div> */}
+							<div className="circle-logo-regular"><i class="fa-solid fa-circle-dot"></i></div>
+							<div className="circle-logo-solid"><i class="fa-solid fa-circle"></i></div>
+						</div>
+
+						<div className="circle-logo">
+							{/* <div className="circle-logo-regular"><i class="fa-regular fa-circle"></i></div> */}
+							<div className="circle-logo-regular"><i class="fa-solid fa-circle-dot"></i></div>
+							<div className="circle-logo-solid"><i class="fa-solid fa-circle"></i></div>
+						</div>
+					</div>
+				</div>
+
+				<div className="product-img-4">
+					<div className="pro-bg">
+						<div className="pro-sect-1">
+							<div className="pro-tag-4">
+								<div className="up-arrow-icon"><i class="fa-solid fa-caret-up"></i></div>
+								<div className="tag-price">
+									<p>Rs. 699.00</p>
+									<p>Wrapover top  ></p>
+								</div>
+							</div>
+						</div>
+						<div className="pro-sect-2">
+							<div className="pro-text-4">
+								<p>A/W 2023</p>
+								<p>Cooler days call for knits, coats, and boots made for the outdoors.</p>
+							</div>
+							<div className="pro-button">
+								<button className="shop-button">Shop now</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="product-img-5">
+					<div className="pro-bg-5">
+						<div className="pro-sect-1">
+							<div className="pro-tag-5">
+								<div className="tag-price">
+									<p>Rs. 1,799.00</p>
+									<p>Satin shirt  ></p>
+								</div>
+							</div>
+						</div>
+						<div className="pro-sect-2">
+							<div className="pro-text-3">
+								<p>Fashion that delivers</p>
+								<p>Explore layers of autumn</p>
+							</div>
+							<div className="pro-button">
+								<button className="shop-button">Shop Now</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="magazine">
+				<div className="magazine-content">
+					<h1>MAGAZINE</h1>
+					<p>A WORLD OF INSPIRATION</p>
+					<p>READ H&M MAGAZINE</p>
+					<div className="">
+						
 					</div>
 				</div>
 			</div>
