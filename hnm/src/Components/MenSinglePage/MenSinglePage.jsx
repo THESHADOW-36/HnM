49fb924ef4dae136795cd782283f6dca2b89cca0
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-08 01:38:10 +0530

    Updated

--- hnm/src/Components/MenSinglePage/MenSinglePage.jsx
+++ hnm/src/Components/MenSinglePage/MenSinglePage.jsx
@@ -23,82 +23,86 @@ const MenSinglePage = () => {
                <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F4b%2Fd9%2F4bd948f944ef49d8a35a49b6a75a5fd70ef7d913.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVEDETAIL%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D" alt="" /></div>
             </div>
 
-
-
             <div className="men-shirts-details">
-               <div className="men-shirts-name">
-                  <p>Twill overshirt</p>
-                  <div className="men-shirts-heart">
-                     <div><i class="fa-regular fa-heart fa-xl"></i></div>
-                     <div><i class="fa-solid fa-heart fa-xl"></i></div>
+               <div className="men-shirts-position">
+                  <div className="men-shirts-name">
+                     <p>Twill overshirt</p>
+                     <div className="men-shirts-heart">
+                        <div><i class="fa-regular fa-heart fa-xl"></i></div>
+                        <div><i class="fa-solid fa-heart fa-xl"></i></div>
+                     </div>
+
+                  </div>
+                  <div className="men-shirts-price">Rs. 2,699.00</div>
+
+                  <div className="men-shirts-cat">
+                     <p>Black/Checked</p>
+                     <div className="men-shirts-cat-img">
+                        <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F7e%2Fdd%2F7eddd2915739c10b4ec1b19e940dc03075058283.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                        <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F0e%2Fb2%2F0eb2599fc98248891b786f092a230c2f83bc05be.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                        <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fd3%2Fa8%2Fd3a8402beddb224eb4615a05bf10132c455a2156.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                        <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fa2%2F21%2Fa221b6a6b233de799a2329d76ab9e9cc94ea6533.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                        <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fe8%2Fba%2Fe8baa968ff5222c997c2eb3af8c5fbc38f5bbc16.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                     </div>
                   </div>
 
-               </div>
-               <div className="men-shirts-price">Rs. 2,699.00</div>
+                  <div className="size-of-shirts">
+                     
+                  </div>
 
-               <div className="men-shirts-cat">
-                  <p>Black/Checked</p>
-                  <div className="men-shirts-cat-img">
-                     <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F7e%2Fdd%2F7eddd2915739c10b4ec1b19e940dc03075058283.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
-                     <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F0e%2Fb2%2F0eb2599fc98248891b786f092a230c2f83bc05be.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
-                     <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fd3%2Fa8%2Fd3a8402beddb224eb4615a05bf10132c455a2156.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
-                     <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fa2%2F21%2Fa221b6a6b233de799a2329d76ab9e9cc94ea6533.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
-                     <div><img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fe8%2Fba%2Fe8baa968ff5222c997c2eb3af8c5fbc38f5bbc16.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_shirtjackets%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D" alt="" /></div>
+                  <div className="men-shirts-add-button">
+                     <button><i class="fa-solid fa-bag-shopping"></i> Add</button>
                   </div>
-               </div>
 
-               <div className="men-shirts-add-button">
-                  <button><i class="fa-solid fa-bag-shopping"></i> Add</button>
-               </div>
+                  <div className="men-shirts-f-s">
+                     <div className="men-shirts-find-in-store">
+                        <div><i class="fa-solid fa-store"></i></div>
+                        <p>Find in store</p>
+                     </div>
+                     <div className="men-shirts-delivery-duration">
+                        <div className="men-shirts-info-icon">i</div>
+                        <p>Standard delivery in 2-7 days</p>
+                     </div>
+                  </div>
 
-               <div className="men-shirts-f-s">
-                  <div className="men-shirts-find-in-store">
-                     <div><i class="fa-solid fa-store"></i></div>
-                     <p>Find in store</p>
-                  </div>
-                  <div className="men-shirts-delivery-duration">
-                     <div className="men-shirts-info-icon">i</div>
-                     <p>Standard delivery in 2-7 days</p>
-                  </div>
-               </div>
+                  <p className="men-shirts-dap">Delivery and Payment</p>
 
-               <p className="men-shirts-dap">Delivery and Payment</p>
+                  <div className="men-shirts-reviews">
+                     <div className="men-shirts-review-stars">
+                        <div><i class="fa-solid fa-star fa-sm"></i></div>
+                        <div><i class="fa-solid fa-star fa-sm"></i></div>
+                        <div><i class="fa-solid fa-star fa-sm"></i></div>
+                        <div><i class="fa-solid fa-star fa-sm"></i></div>
+                        <div><i class="fa-regular fa-star fa-sm"></i></div>
+                     </div>
+                     <p>(2251 reviews)</p>
+                  </div>
 
-               <div className="men-shirts-reviews">
-                  <div className="men-shirts-review-stars">
-                     <div><i class="fa-solid fa-star fa-sm"></i></div>
-                     <div><i class="fa-solid fa-star fa-sm"></i></div>
-                     <div><i class="fa-solid fa-star fa-sm"></i></div>
-                     <div><i class="fa-solid fa-star fa-sm"></i></div>
-                     <div><i class="fa-regular fa-star fa-sm"></i></div>
+                  <div className="men-shirts-size">
+                     <p>True to Size</p>
+                     <div className="men-shirts-ajustment">
+                        <i class="fa-solid fa-circle fa-2xs"></i>
+                     </div>
+                     <div className="men-shirts-size-range">
+                        <p>Small</p>
+                        <p>Spot on</p>
+                        <p>Large</p>
+                     </div>
                   </div>
-                  <p>(2251 reviews)</p>
-               </div>
 
-               <div className="men-shirts-size">
-                  <p>True to Size</p>
-                  <div className="men-shirts-ajustment">
-                     line
-                  </div>
-                  <div className="men-shirts-">
-                     <p>Small</p>
-                     <p>Spot on</p>
-                     <p>Large</p>
-                  </div>
-               </div>
-
-               <div className="men-shirts-summary">
-                  <div className="men-shirts-description">
-                     <p>Description & fit</p>
-                     <div><i class="fa-solid fa-angle-down"></i></div>
-                  </div>
-                  <div className="men-shirts-description">
-                     <p>Materials & suppliers</p>
-                     <div><i class="fa-solid fa-angle-down"></i></div>
-                  </div>
-                  <div className="men-shirts-description">
-                     <p>Care guide</p>
-                     <div><i class="fa-solid fa-angle-down"></i></div>
+                  <div className="men-shirts-summary">
+                     <div className="men-shirts-description">
+                        <p>Description & fit</p>
+                        <div><i class="fa-solid fa-angle-down"></i></div>
+                     </div>
+                     <div className="men-shirts-description">
+                        <p>Materials & suppliers</p>
+                        <div><i class="fa-solid fa-angle-down"></i></div>
+                     </div>
+                     <div className="men-shirts-description">
+                        <p>Care guide</p>
+                        <div><i class="fa-solid fa-angle-down"></i></div>
+                     </div>
                   </div>
                </div>
             </div>
