4e99a30ed167d92b91f27926d7058fec3fcd8709
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-18 15:20:35 +0530

    Updated

---
+++ apple/src/Component/Footer/Footer4.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Footer4.css"
+
+const Footer4 = () => {
+   return (
+      <div className="footer4">
+         <div className="footer4-content-text-2">
+            <p className="footer4-more-ways-to-shop">More ways to shop: <span>Find a retailer</span> near you. Or call 000800 040 1966.</p>
+            <div className="footer4-pp-tc">
+               <div className="footer4-copyright-pp">
+                  <p>Copyright © 2023 Apple Inc. All rights reserved.</p>
+                  <div className="footer4-pp-t-sp-l-sm">
+                     <p>Privacy Policy</p>
+                     <div className="vertical-line"></div>
+                     <p>Terms of Use</p>
+                     <div className="vertical-line"></div>
+                     <p>Sales Policy</p>
+                     <div className="vertical-line"></div>
+                     <p>Legal</p>
+                     <div className="vertical-line"></div>
+                     <p>Site Map</p>
+                  </div>
+               </div>
+               <p>India</p>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Footer4;
--- apple/src/Component/Sign-up/SignUp.jsx
+++ apple/src/Component/Sign-up/SignUp.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./SignUp.css"
 import Navbar3 from "../Header/Navbar3";
+import img1 from "./../../Images/Access-logo.png"
+import Footer4 from "../Footer/Footer4";
 
 const SignUp = () => {
    return (
@@ -54,7 +56,7 @@ const SignUp = () => {
                </div>
             </div>
 
-            <div className="su-vertical-line-layout"><div className="su-vertical-line"></div></div>
+            <div className="su-horizontal-line-layout"><div className="su-horizontal-line"></div></div>
 
             <div className="su-create-ur-id-layout">
                <div className="su-input-box"><input type="text" placeholder="name@example.com" /></div>
@@ -63,7 +65,7 @@ const SignUp = () => {
                <div className="su-input-box"><input type="text" placeholder="confirm password" /></div>
             </div>
 
-            <div className="su-vertical-line-layout"><div className="su-vertical-line"></div></div>
+            <div className="su-horizontal-line-layout"><div className="su-horizontal-line"></div></div>
 
             <div className="su-create-ur-id-layout">
                <div className="su-gap"></div>
@@ -332,13 +334,13 @@ const SignUp = () => {
                </div>
             </div>
 
-            <div className="su-vertical-line-layout"><div className="su-vertical-line"></div></div>
+            <div className="su-horizontal-line-layout"><div className="su-horizontal-line"></div></div>
 
             <div className="su-create-ur-id-layout">
                <div className="su-announcements-apps">
-                  
+
                   <div className="su-announcements">
-                     <input type="checkbox" />
+                     <div className="su-checkbox"><input type="checkbox" defaultChecked /></div>
                      <div className="su-announcements-text">
                         <p>Announcements</p>
                         <p>Receive Apple emails and communications including announcements, marketing, recommendations and updates about Apple products, services and software.</p>
@@ -346,7 +348,7 @@ const SignUp = () => {
                   </div>
 
                   <div className="su-apps">
-                     <input type="checkbox" />
+                     <div className="su-checkbox"><input type="checkbox" defaultChecked /></div>
                      <div className="su-apps-text">
                         <p>Apps, Music, TV and More</p>
                         <p>Receive Apple emails and communications including new releases, exclusive content, special offers and marketing, and recommendations for apps, music, movies, TV, books, podcasts and more.</p>
@@ -355,12 +357,37 @@ const SignUp = () => {
                </div>
             </div>
 
-            <div className="su-footer">
+            <div className="su-horizontal-line-layout"><div className="su-horizontal-line"></div></div>
 
+            <div className="su-create-ur-id-layout">
+               <div className="su-captcha">
+                  <div className="su-captcha-img"><img src="data:image/jpeg;base64, /9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCABGAKADASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDpIoovIsCdNyTjJ2x/vPkPv+PPpRLFF5F+RpuCM4O2P938g9/x49aIpYvIsAdSwRjI3R/u/kPt+HPrRLLF5F+BqWSc4G6P958g9vw49KAODm+J+maf4ju7HVNB8uO1mlg3wBWLYbAJU4GcD1PWu3tZLW80qG6h0/dHNIro+xBlTJkDrnocf/WrwDxyix+N9XCyeYDcFt+Qc5wc8cd69t8Kzxv4H0Ytf7G8qHMe5OMMOeRn3/8ArUAbqww/bZR/ZWR5afLtj45bnr3/AKVDFFF5FgTpuScZO2P958h9/wAefSqHiDxFZ+HbK71CXUWkKxIsSI0ZaV8thR8v4n0BNeb6D8U9anvbOyubRbn94EiEBWNhwV7gjgHv2/OgD1eWKLyL8jTcEZwdsf7v5B7/AI8etTNDD9tiH9lYHlv8u2Pnleevb+tQSSxG2v8AOo5Jzhd0fz/ux7fhx6VO00P22I/2rkeW/wA26PjleOnf+lAEPlRfZc/2bz9oxu2x9PN+71/D0/Cplhh+2yj+ysjy0+XbHxy3PXv/AEqHzYvsuP7S5+0Z27o+nm/e6fj6fhUyzQ/bZT/auB5afNuj55bjp2/rQBDFFF5FgTpuScZO2P8AefIff8efSiWKLyL8jTcEZwdsf7v5B7/jx60RSxeRYA6lgjGRuj/d/Ifb8OfWiWWLyL8DUsk5wN0f7z5B7fhx6UATNDD9tiH9lYHlv8u2Pnleevb+tQ+VF9lz/ZvP2jG7bH0837vX8PT8KmaaH7bEf7VyPLf5t0fHK8dO/wDSofNi+y4/tLn7Rnbuj6eb97p+Pp+FAEyww/bZR/ZWR5afLtj45bnr3/pUMUUXkWBOm5Jxk7Y/3nyH3/Hn0qZZoftsp/tXA8tPm3R88tx07f1qGKWLyLAHUsEYyN0f7v5D7fhz60AEsUXkX5Gm4Izg7Y/3fyD3/Hj1qZoYftsQ/srA8t/l2x88rz17f1qGWWLyL8DUsk5wN0f7z5B7fhx6VM00P22I/wBq5Hlv826PjleOnf8ApQBD5UX2XP8AZvP2jG7bH0837vX8PT8KmWGH7bKP7KyPLT5dsfHLc9e/9Kh82L7Lj+0uftGdu6Pp5v3un4+n4VMs0P22U/2rgeWnzbo+eW46dv60AELXP2bTcRRYGNv708/u26/Lxx9aJmufs2pZiiwc7v3p4/dr0+Xnj6VDF5HkWGfteeN2PNx9w9P/AK3b2ol8jyL/AB9rzztz5uPuDr/9ft7UAeFfE+N4/iDqRkVVL+U2FbI/1a98CvV/B140Hwz0u4l8lLaJQzSPKRgLL1Py8Dj1rzL4sxJH42ZoxLtkto2/ebs9x/Fz2rNvfFckngnTfDtqZUWMO12284cmRmVQM4wBgn3+nIAeOPFsvi3XGuAPLtIR5dvHn+H+8fc/4CkhfVfAd9p19F5SX1xbfaFWRA+yNyQAQehIBP0Ird+GPhCLVb/+1tTilNlbkGKNY2Pmvk88D7oI/E/Q1i/EK6Fz4umVSxWGGGIbiT0QE9e2SaAN6D4x6wIriO60+ymE4O4puQ/dC+p9K6XTvjLp15fwm9097L5WTeZd6DJHJIXPb0NU/A/hPQdQ8CfatT0zz7tzKyybXyB/DyvHbPPrXLfEnwtpnhrUbRtKMyQXSuTBKrZjKkdCwyQc+/Q0Aeq614z0/Q9Ciu55IJVnlMsCQzbmmAk3ZXjGPcn9eKwoPF3jzWp3utF8LwQwPGuPtbclctgglkznJ6DtXH/DHQ7PV764vdQEsyWDQrDFtZly7kk8dMYOB3Lete1r9m+2S/8AH5jy0x/rs9W/H/J96APPV+IOv6C2nx+J/D4gtsjy7iAnaRtI9WB4OeDn2rvIr1r7TL26tvs8tvMpcSJKSCDGvI+Xnj6Vma5pllrHhlbGZLhvMi/iWQhWEbFSvbhsHj+VcP8ADDX7GPw7e6Ve3hjuPMb7LH5jDfuXkBRweR39aAPWHe6+3w/uYd3lPgeacYyn+z9Kg3XP2P8A1UWPtPXzT187/d9f89qG+zfbYv8Aj8x5b5/12eq/j/ke1Q/uPsv/AC95+0f9NcY838s4/HPvQBdR7r7fN+5h3eUmR5pxjL/7P1qCFrn7NpuIosDG396ef3bdfl44+teZ/EjV71vE+maLod5ewTyBC5SSRX3MxCj14BJ/GvQ7VIIrLTIy165VVVmJlJOIzyP/AK38qALMzXP2bUsxRYOd3708fu16fLzx9Knd7r7fD+5h3eU+B5pxjKf7P0qhcyWsVnqUjtdKqKzZbzcACMfe/wDr9vauf8I+MLbxZdzsmn3ttFboRv8APkl3EkYxjkcDn6j2oA6jdc/Y/wDVRY+09fNPXzv931/z2qdHuvt837mHd5SZHmnGMv8A7P1ql+4+y/8AL3n7R/01xjzfyzj8c+9TL9m+2y/8fmPLTH+uz1b8f8n3oAIVufs2m4liwcbf3R4/dt1+bnj6UTLc/ZtSzLFgZ3fujz+7Xp83HH1qGKKLyLAnTck4ydsf7z5D7/jz6USxReRfkabgjODtj/d/IPf8ePWgDifiL4B1fxNrNve2U9q8q2vlmNgY8hWJ4PIz8/fHSuC0n4ca7da3FaX9lNaWwcCa4IBUDcFIVhwTk9M+9e8tDD9tiH9lYHlv8u2Pnleevb+tQ+VF9lz/AGbz9oxu2x9PN+71/D0/CgCawsX09vsdobeKGGCNEVYTgKC2B976896+ZNcuTea7fTk5Dzvj6ZwP0xX0neyW9lFfXTaYAkFr5pBSPC43knr7fpXzJaQPe38FuuS88qoMdSWOP60Ab1trni/QbI28c+oWtqVxsliO3BHYMPTFZGq6xqOuXZvNSupbmbG3e/QD0AHAHXgV9KvBClrfKum7QgIU7U/d/ux7/jx61zHxK0N9V8NOllpzRzW4NyEVUBYKQD9084UmgBPhzoEukeC4LoTRGTUJY52BjJKjcAozkdOvTua7ZEuvt8376Hd5SZPlHGMv/tfWuF+H+tafqvhC0tFt0kvrMpFMMJuwHAU8nJBGBnpniuxK20VzO8mmqsaRKx3CMBeWyT82P/1UAeNeNL3xhc3F3bSSXCaRaBdrRr5cW0oCAW/iODjGT9K4Oyumsr63ukzuhkVx+BzXpXibVz43vbDwx4ctlZVw1zcBQA7AcnP90cnnqcegzy/j7QIvD3iFbW3idIDbx7d+MlgoDE4JGSRn/gVAH0KjTz3NtLHcQMskDMjCI4Kkp/tfSon8+OwZ3nhWNbjJJjPH73rnd0zz/nNc14AvIdT8K6S5sRK8UDwSYVPmKMoB5Ppjr61D4/vY9N8B3zJZeXLPKYEl2p8uXORwc/dBFAHM+BY5/F3xJ1PxG7JstiWjLoSMtlEGM/3Qe/UV6lCtz9m03EsWDjb+6PH7tuvzc8fSuN+F2kR2PhhJZbEzyXaickhDgEsBjJ6YA/Wusiii8iwJ03JOMnbH+8+Q+/48+lAHJ/E3Wp9J8L3tqJ4/Ov5xBhUIO3YpYjnpjC/8Cq/8PtBn0Tw5p0e6NJ7mJ7mTdGSQWKYB5HONv5GuC8UKPFXxOGl20O20s8iRFC4UKuZOhx1GPrXsDQwC8iA0rA8t/k2R88rz1xx/WgA23P2P/WxY+09PKPXzv971/wA96nRLr7fN++h3eUmT5RxjL/7X1ql5UX2XP9m8/aMbtsfTzfu9fw9PwqZYYftso/srI8tPl2x8ctz17/0oAhili8iwB1LBGMjdH+7+Q+34c+tEssXkX4GpZJzgbo/3nyD2/Dj0qaFrn7NpuIosDG396ef3bdfl44+tEzXP2bUsxRYOd3708fu16fLzx9KABpoftsR/tXI8t/m3R8crx07/ANKh82L7Lj+0uftGdu6Pp5v3un4+n4Vdd7r7fD+5h3eU+B5pxjKf7P0qDdc/Y/8AVRY+09fNPXzv931/z2oAazW0txOkupK0TxKrbjEQ4y2VOVwf/r1hR+F/DQl027WG0iuI3STdGsaFGClgeB/eA65rpke6+3zfuYd3lJkeacYy/wDs/WoIWufs2m4iiwMbf3p5/dt1+Xjj60AQyyxeRfgalknOBuj/AHnyD2/Dj0qZpoftsR/tTI8t/m3R8crx07/0oma5+zalmKLBzu/enj92vT5eePpU7vdfb4f3MO7ynwPNOMZT/Z+lAHAap8O9CvJm1Cy1OXT75pzkwSKF+/jcFGMcc8ECuI8d+HtQ0M28k2vXOs2L4EjNKcqcnC8s3YHnt/P2/dc/Y/8AVRY+09fNPXzv931/z2pt9YjVftdlfWVvPBNDGJI2mbBG5iOQuc5/LFAHP+EbXQLTQdNn0a5WGOfDykshcNsbO4kZyDkc8c8VyfxftIJrC2v0u1mliujFjcuWVo1O7jqAVxXaQeHlsPDQsNGgFmbpGZHjunyJGhYBhkfKe/B7V5pD8KPEd+J7vU723haPmTzJDLJ90N24PB9aAIfh/wCN7PwxZXcF+92E3GSFbdFYlm2hh83A+6K6l9StviT4N1O0hla1uILlWhjnkTDEtkMcKCOrA4zjr7VNp3wg0uzuYlvt985RmKm4MaHBX+6uR19a63TNGtdF09k07TLO2VrhQzI53NtlwATtyQOnJ/woA878MeOZPCluNH8RQX0BgURRSwIjgqCSAc9QM8EE1a1T4owPp9va+HxfXGohQE3wrsjO0jgYyxGa9OKzy3kyyW1u4MSZVpCRjL/7P1qraQNb2+n+RZ2secYKNt3fu264X/GgDifAnhcaJpmoahqN4o1a6RlZBIjHaQGxnnJJPOPSu7aaH7bEf7VyPLf5t0fHK8dO/wDSiZrn7NqWYosHO796eP3a9Pl54+lTu919vh/cw7vKfA804xlP9n6UAUvNi+y4/tLn7Rnbuj6eb97p+Pp+FTLND9tlP9q4Hlp826PnluOnb+tG65+x/wCqix9p6+aevnf7vr/ntU6Pdfb5v3MO7ykyPNOMZf8A2frQBBDbRm2007pfmxn98/8AzzY8c8fhRNbRi21I7pflzj98/wDzzU888/jRRQBO9pH9vhXdNgxOf9c+eqd81B9mj+x53S5+04/1z/8APbHr+v40UUATpaR/b5l3TYESH/XPnq/fNQQ20ZttNO6X5sZ/fP8A882PHPH4UUUAE1tGLbUjul+XOP3z/wDPNTzzz+NTvaR/b4V3TYMTn/XPnqnfNFFAEH2aP7HndLn7Tj/XP/z2x6/r+NTpaR/b5l3TYESH/XPnq/fNFFAEENtGbbTTul+bGf3z/wDPNjxzx+FE1tGLbUjul+XOP3z/APPNTzzz+NFFAE72kf2+Fd02DE5/1z56p3zUH2aP7HndLn7Tj/XP/wA9sev6/jRRQBOlpH9vmXdNgRIf9c+er981BDbRm2007pfmxn98/wDzzY8c8fhRRQATW0YttSO6X5c4/fP/AM81PPPP41O9pH9vhXdNgxOf9c+eqd80UUAQfZo/sed0uftOP9c//PbHr+v41OlpH9vmXdNgRIf9c+er980UUAf/2Q==" alt="" /></div>
+                  <div className="su-captcha-box">
+                     <div className="su-captcha-input"><input type="text" placeholder="Type the characters in the image" /></div>
+                     <div className="su-captcha-new-code">
+                        <p>New code</p>
+                        <div className="su-captcha-vertical-line"></div>
+                        <p>Visually-impaired</p>
+                     </div>
+                  </div>
+               </div>
             </div>
+
+            <div className="su-horizontal-line-layout"><div className="su-horizontal-line"></div></div>
+
+            <div className="su-create-ur-id-layout">
+               <div className="su-access-ur-data">
+                  <div className="su-access-ur-data-logo"><img src={img1} alt="" /></div>
+                  <p className="su-access-ur-data-text">Your Apple ID information is used to allow you to sign in securely and access your data. Apple records certain data for security, support and reporting purposes. If you agree, Apple may also use your Apple ID information to send you marketing emails and communications, including based on your use of Apple services. <span>See how your data is managed.</span></p>
+                  <div className="su-access-ur-data-button"><button>Continue</button></div>
+               </div>
+            </div>
+
+
+            <div className="su-footer"></div>
          </div >
 
-
+         <Footer4/>
       </>
    )
 }
