5363d7d9657c041b7ce1a53ffc1751c8c1633058
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-27 15:19:35 +0530

    Updated

---
+++ youtube/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Components/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ youtube/src/Components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+   return(
+      <div>
+         hello YT
+      </div>
+   )
+}
+
+export default Homepage;
